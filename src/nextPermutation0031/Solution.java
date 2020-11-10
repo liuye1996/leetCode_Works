1a20cfd5106e6b30accd2cebad352bc386509d8b
ye_liu <ye_liu@fpi-inc.com>
2020-11-10 16:01:49 +0800

    feat: add 0031

---
+++ src/nextPermutation0031/Solution.java
@@ -0,0 +1,56 @@
+package nextPermutation0031;
+
+/**
+ * @Author: ye_liu
+ * @Date: 2020/11/10 14:44
+ */
+public class Solution {
+
+    /**
+     * https://leetcode-cn.com/problems/next-permutation/
+     *
+     * 31. 下一个排列
+     * 实现获取下一个排列的函数，算法需要将给定数字序列重新排列成字典序中下一个更大的排列。
+     *
+     * 如果不存在下一个更大的排列，则将数字重新排列成最小的排列（即升序排列）。
+     *
+     * 必须原地修改，只允许使用额外常数空间。
+     *
+     * 以下是一些例子，输入位于左侧列，其相应输出位于右侧列。
+     * 1,2,3 → 1,3,2
+     * 3,2,1 → 1,2,3
+     * 1,1,5 → 1,5,1
+     *
+     * 通过次数108,393提交次数305,024
+     *
+     * @param nums
+     */
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i+1]){
+            i--;
+        }
+        if (i>=0){
+            int j = nums.length - 1;
+            while(j >= 0 && nums[i] >= nums[j]){
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        int left = i + 1;
+        int right = nums.length - 1;
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+
+    private void swap(int[] nums,int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+
+}
