24a9e5123391840d50298df57145e83348a639a0
ye_liu <ye_liu@fpi-inc.com>
2020-12-21 14:49:25 +0800

    feat: add 0118、0048

---
+++ src/pascalsTriangle0118/Solution0118.java
@@ -0,0 +1,53 @@
+package pascalsTriangle0118;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: ye_liu
+ * @Date: 2020/12/6 11:32
+ */
+public class Solution0118 {
+
+
+    /**
+     * 118. 杨辉三角
+     * 给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+     *
+     * 在杨辉三角中，每个数是它左上方和右上方的数的和。
+     *
+     * 示例:
+     *
+     * 输入: 5
+     * 输出:
+     * [
+     *      [1],
+     *     [1,1],
+     *    [1,2,1],
+     *   [1,3,3,1],
+     *  [1,4,6,4,1]
+     * ]
+     * 通过次数124,353提交次数181,639
+     * @param numRows
+     * @return
+     */
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        for (int i = 0; i < numRows; ++i) {
+            List<Integer> row = new ArrayList<Integer>();
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(ret.get(i - 1).get(j - 1) + ret.get(i - 1).get(j));
+                }
+            }
+            ret.add(row);
+        }
+        return ret;
+    }
+
+
+
+
+}
